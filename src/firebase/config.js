0fd2cfdf58317ddce9407d17c7dcff50def68e77
Văn Viet <vietvo371@gmail.com>
2025-03-18 17:01:51 +0700

    fix key

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -8,13 +8,13 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAfCptH_y5TA3IP0bbEGRQ1mXwhDzeZCss",
-  authDomain: "login-c5a4c.firebaseapp.com",
-  projectId: "login-c5a4c",
-  storageBucket: "login-c5a4c.firebasestorage.app",
-  messagingSenderId: "944810457078",
-  appId: "1:944810457078:web:6e3a8a2cd57b7f0dc679d7",
-  measurementId: "G-ZB9TC2SZ01"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
